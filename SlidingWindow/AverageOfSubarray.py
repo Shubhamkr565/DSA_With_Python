750673e6c7ab169896b4e745829940100bc6dcfd
Shubham <shubhamkr565@gmail.com>
2026-05-15 10:24:07 +0530

    Sliding Window

--- SlidingWindow/AverageOfSubarray.py
+++ SlidingWindow/AverageOfSubarray.py
@@ -1,18 +1,41 @@
 # Average of Subarray of size k
 
-array = [1,4,5,7]
+# array = [1,4,5,7]
 
-k = 2
-avg_max_sum = 0
+# k = 2
+# avg_max_sum = 0
 
-for i in range(len(array)-k+1):
+# for i in range(len(array)-k+1):
+#     window_sum = 0
+#     for j in range(i, i+k):
+#         window_sum += array[j]
+        
+#         avg = window_sum/k
+#         avg_max_sum = max(avg_max_sum, avg)
+# print(avg_max_sum)
+
+
+
+
+
+array = [6,4,7,8,3,1]
+k = 3
+left = 0
+avg = 0
+
+max_avg = 0
+
+for i in range(len(array) - k+1):
     window_sum = 0
+    
     for j in range(i, i+k):
         window_sum += array[j]
-        
-        avg = window_sum/k
-        avg_max_sum = max(avg_max_sum, avg)
-print(avg_max_sum)
+
+    avg = window_sum/k
+    print(f"{array[i: i+k]}", " => ", round(avg, 2))
+    max_avg = max(max_avg, avg)
+
+print(max_avg)
 
 
 
--- SlidingWindow/ex.py
+++ SlidingWindow/ex.py
@@ -114,27 +114,161 @@
 
 # Count vowels in every substring of size K
 
-s = "shubham"
+# s = "shubham"
 
-k = 3
-count = 0
-vowel = "aeiouAEIOU"
-for i in range(k):
-    if s[i] in vowel:
-        count += 1
-print(f'"{s[0:k]}" → {count} vowel')
-for j in range(k, len(s)):
-    # remove outgoing char
-    if s[j-k] in vowel:
-        count -= 1
+# k = 3
+# count = 0
+# vowel = "aeiouAEIOU"
+# for i in range(k):
+#     if s[i] in vowel:
+#         count += 1
+# print(f'"{s[0:k]}" → {count} vowel')
+# for j in range(k, len(s)):
+#     # remove outgoing char
+#     if s[j-k] in vowel:
+#         count -= 1
     
-    # add incoming char
+#     # add incoming char
 
-    if s[j] in vowel:
+#     if s[j] in vowel:
+#         count += 1
+
+#     window = s[j-k+1 : j+1]
+#     print(f'"{window}" → {count} vowel')
+
+
+
+
+
+
+# Maximum sum subarray of size K. 
+
+# array = [6,4,7,8,3,1]
+# k = 3
+# left = 0
+# max_sum = 0
+
+# for i in range(len(array)- k+1):
+#     window_sum  = 0
+#     for j in range(i, i+k):
+#         window_sum += array[j]
+
+#     max_sum = max(window_sum, max_sum)
+
+# print(max_sum)
+
+# array = [6,4,7,8,3,1]
+# k = 3
+# left = 0
+# max_sum = 0
+# window_sum = 0
+
+# for right in range(len(array)):
+#     window_sum += array[right]
+
+#     if right - left+1 == k:
+#         max_sum = max(max_sum,  window_sum)
+
+#         window_sum -= array[left]
+
+#         left += 1
+    
+# print(max_sum)
+
+
+
+
+
+# Average of subarray of size k
+
+# array = [6,4,7,8,3,1]
+# k = 3
+# left = 0
+# avg = 0
+
+# max_avg = 0
+
+# for i in range(len(array) - k+1):
+#     window_sum = 0
+    
+#     for j in range(i, i+k):
+#         window_sum += array[j]
+
+#     avg = window_sum/k
+#     print(f"{array[i: i+k]}", " => ", round(avg, 2))
+#     max_avg = max(max_avg, avg)
+
+# print(max_avg)
+
+
+
+
+
+#  Maximum sum subarray of size K. 
+
+# array = [6,4,7,8,3,1]
+# k = 3
+
+# window_sum = 0
+# max_sum = 0
+
+# left = 0
+
+# for right in range(len(array)):
+#     window_sum += array[right]
+
+#     if right - left+1 == k:
+#         print(f"{array[left: right+1]}", " => ", window_sum)
+#         max_sum = max(max_sum, window_sum)
+
+#         window_sum -= array[left]
+
+#         left += 1
+    
+
+# print(max_sum)
+
+
+
+
+# Count vowels in every substring of size K
+
+
+str = "Shubham"
+k = 3
+vowel = "aeiouAEIOU"
+left = 0
+window_vowerl = []
+count = 0
+
+for right in range(len(str)):
+    if str[right] in vowel:
+        window_vowerl.append(str[right])
         count += 1
 
-    window = s[j-k+1 : j+1]
-    print(f'"{window}" → {count} vowel')
+    if right - left+1 == k:
+        print(f"{str[left: right+1]}", " => ", window_vowerl,"=>", count)
+        
+        if str[left] in vowel:
+            window_vowerl.remove(str[left])
+            count -= 1
+
+        left += 1
 
 
 
+
+
+
+
+
+# str = "Shubham"
+# k = 3
+# vowel = "aeiouAEIOU"
+
+# for i in range(len(str) - k+1):
+#     count = 0
+#     for j in range(i, i+k):
+#         if str[j] in vowel:
+#             count +=1
+#     print(f"{str[i:i+k]}", " => ", count)
